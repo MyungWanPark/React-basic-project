7c508a67416a0b714139797aba79320743a9efa3
MyungWanPark <mywanpark@gmail.com>
2021-12-01 15:09:48 +0900

    Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
-
+import { Link } from "react-router-dom";
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt="" />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((genre) => (
