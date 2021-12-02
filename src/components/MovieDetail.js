53b16aaaa5dae1696d3e5603342a6a04560febb6
MyungWanPark <mywanpark@gmail.com>
2021-12-02 13:34:45 +0900

    code MovieDetail

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-function Movie({ id, coverImg, title, summary, genres }) {
+function MovieDetail({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt="" />
       <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
+        <Link to={"/"}>{title}</Link>
       </h2>
-      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+      <p>{summary}</p>
       <ul>
         {genres.map((genre) => (
           <li key={genre}>{genre}</li>
@@ -17,11 +17,10 @@ function Movie({ id, coverImg, title, summary, genres }) {
   );
 }
 
-Movie.propTypes = {
-  id: PropTypes.number.isRequired,
+MovieDetail.propTypes = {
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
-export default Movie;
+export default MovieDetail;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,41 @@
-import { useCallback, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import MovieDetail from "../components/MovieDetail";
 
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [loading, setLoading] = useState(true);
   const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
   }, [id]);
 
   useEffect(() => {
     getMovie();
   }, [getMovie]);
-  return <h1>Detail</h1>;
+
+  return (
+    <div>
+      <Link to={"/"}>Home</Link>
+      <h1>Detail</h1>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <div>
+          <MovieDetail
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.description_full}
+            genres={movie.genres}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
